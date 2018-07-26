085b701ebbb72666cd3494cadd4654780fa47077
jeff-burns <1jeffreyburns@gmail.com>
2018-07-25 20:45:38 -0600

    added bit to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,12 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/headlines' //db name
+    client: "pg",
+    connection: "postgres://localhost/headlines" //db name
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL //when on heroku, tells it to look here
+    client: "pg",
+    connection: process.env.DATABASE_URL + "?ssl=true" //when on heroku, tells it to look here
   }
 };
